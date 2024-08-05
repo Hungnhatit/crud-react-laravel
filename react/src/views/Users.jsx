912e629d534d730a9a9f15d6f1c63d4fe27013b4
Hungnhatit <macbookair@Macbooks-MacBook-Air.local>
2024-08-05 17:19:43 +0700

    implement user api

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,27 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import axiosClient from "../axios-client";
+
 export default function User() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = () => {
+    setLoading(true);
+    axiosClient.get('/users')
+      .then(({ data }) => {
+        setLoading(false);
+        console.log(data);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }
+
   return (
     <div>
       <h1>User</h1>
